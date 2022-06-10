d1be9e767f182b3235978cfa8d2ef5ea1b7338fb
Alessandra Cabral <99781101+AlessandraCaabral@users.noreply.github.com>
2022-06-09 22:22:31 -0300

    Resolvendo o exercicio JS

---
+++ exercicio.js
@@ -0,0 +1,42 @@
+# Resolução do exercício #
+
+ <meta charset="utf-8">
+
+<script>
+
+	function pulalinha() {
+		document.write("<br><br>")
+	}
+
+	function mostra(frase){
+
+		document.write(frase)
+		pulalinha()
+	}
+
+function sorteia(n){
+    return Math.round(Math.random() * n);
+
+}
+
+var numeroPensado = sorteia(20); 
+
+var chute = parseInt(prompt("Estou com um número na minha cabeça. Caro aventureiro, quer se arriscar em um bom chute? Pense em um número de 0 até 20")); 
+
+	if (chute == numeroPensado) {
+
+		mostra("BOAAAAAAAA AVENTUREIRO, VOCÊ ACERTOU!!! Eu estava mesmo pensando no número" + numeroPensado);                                                                                                                           
+	} else {
+
+		if (chute > numeroPensado) {
+
+			mostra("Você errou! Seu chute é maior que o número pensado!"); 
+
+	
+	} else
+	{
+
+		mostra(" Eitaaaaa você errou ! ;/ Eu tinha pensando no número ... " + numeroPensado); 
+	}
+}
+</script>
